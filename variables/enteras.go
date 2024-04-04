ac34ef51eff481a77041e574199eb2119c6f8294
emic310 <80928086+emic310@users.noreply.github.com>
2024-04-04 00:00:38 -0300

    create a public function on variables package

--- variables/enteras.go
+++ variables/enteras.go
@@ -1,2 +1,21 @@
 package variables
 
+import "fmt"
+
+// al empezar con mayusculas la funcion le estoy indicando que es publica 
+func MuestroEnteros() {
+
+	// manera declarativa, no se define en null, para int se define en Cero
+	var intComun int
+
+	// manera asignativa, le asigno lo que retorna la funcion int32(10)
+	// o sea que va a tener el valor y tipo de dato que retorne la funcion
+	intDe32 := int32(10)
+	intDe64 := int64(100)
+
+	// no es lo mismo int, que int32 o int64
+
+	fmt.Println("intComun = ", intComun)
+	fmt.Println("intDe32 = ", intDe32)
+	fmt.Println("intDe64 = ", intDe64)
+}
