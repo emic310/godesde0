87e5a92100be9f717af5c543f66a202eb047787b
emic310 <80928086+emic310@users.noreply.github.com>
2024-04-04 11:17:23 -0300

    fix condition excercise-02

--- ejercicios/ejercicio02.go
+++ ejercicios/ejercicio02.go
@@ -11,14 +11,14 @@ var numero1 int
 var err error
 var salida string
 
-func TablaUnoAlDiez() {
+func TablaCeroAlDiez() {
 	
 	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Println("Ingrese un número entero : ")
 	if scanner.Scan() {
 		
-		for numero1, err = strconv.Atoi(scanner.Text()); err == nil; numero1, err = strconv.Atoi(scanner.Text()){
+		for numero1, err = strconv.Atoi(scanner.Text()); err != nil; numero1, err = strconv.Atoi(scanner.Text()){
 		
 			fmt.Println("Lo ingresado no es un número entero, intente nuevamente: ")
 			if scanner.Scan() {
@@ -31,7 +31,7 @@ func TablaUnoAlDiez() {
 
 	fmt.Println("Tabla del ", numero1)
 
-	for i := 1; i <= 10; i++ {
+	for i := 0; i <= 10; i++ {
 		salida = strconv.Itoa(numero1) + " * " + strconv.Itoa(i) + " = "
 		fmt.Println(salida, numero1 * i)
 	}
